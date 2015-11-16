b8afcb46961016ac27990df366005856558473dd
Qian He <github@heqian.name>
2015-11-16 18:29:48 -0500

    Version 0.0.2
    1. Remove Adafruit_Python_DHT dependency, and use node module "node-dht22" instead;
    2. Upgrade Chart.min.js;
    3. Fix bugs in index.html.

--- server.js
+++ server.js
@@ -1,125 +1,117 @@
 var DEBUG = false;
 
-var http = require('http');
-var fs  = require('fs');
-var path = require('path');
-var mime = require('mime');
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
+var mime = require("mime");
+var dht22 = require("node-dht22");
 var cache = {};
 
-var NeDB = require('nedb');
+var NeDB = require("nedb");
 var nedb = new NeDB({
-    filename: __dirname + '/database.db',
-    autoload: true
+	filename: __dirname + "/database.db",
+	autoload: true
 });
-var exec = require('child_process').exec;
 
-
-// Sensor polling
 function readSensorData() {
-    exec(__dirname + '/dht 22 4', function(error, stdout, stderr) {
-        var regex = /^Temp =  (-?[0-9]+\.[0-9]) \*C, Hum = ([0-9]+\.[0-9]) \%$/mi;
-        if (stdout) {
-            if (regex.test(stdout)) {
-                var result = stdout.match(regex);
-                var temperature = parseFloat(result[1]);
-                var humidity = parseFloat(result[2]);
-                if (-40 <= temperature && temperature <= 80 && 0 <= humidity && humidity <= 100)
-                    saveLog(temperature, humidity);
-            } else {
-                readSensorData();
-            }
-        }
+	var result = dht22.read(4);
+	if (-40 < result.temperature && result.temperature < 80 &&
+		0 < result.humidity && result.humidity < 100 &&
+		Math.round(result.humidity + result.humidity) !== 0) {
+		var log = {
+			"temperature": result.temperature.toFixed(1),
+			"humidity": result.humidity.toFixed(1),
+			"timestamp": new Date()
+		};
 
-        if (stderr) {
-            if (DEBUG) console.log('stderr: ' + stderr);
-        }
-
-        if (error) {
-            if (DEBUG) console.log('error: ' + error);
-        }
-    });
-}
-
-function saveLog(temperature, humidity) {
-    var log = {
-        "temperature": temperature,
-        "humidity": humidity,
-        "timestamp": new Date()
-    };
-
-	nedb.insert(log, function(error, newLog) {
-		if (error) {
-			if (DEBUG) console.log("NeDB error: " + error);
-		}
-	});
+		nedb.insert(log, function(error) {
+			if (error) {
+				if (DEBUG) console.log("NeDB error: " + error);
+			}
+		});
+	} else {
+		readSensorData();
+	}
 }
 
 setInterval(readSensorData, 60000);
 
-
-// Server behaviors
 function send404(response) {
-    response.writeHead(404, {'Content-Type': 'text/plain'});
-    response.write('Error 404: resource not found.');
-    response.end();
+	response.writeHead(404, {
+		"Content-Type": "text/plain"
+	});
+	response.write("Error 404: resource not found.");
+	response.end();
 }
 
 function sendFile(response, filePath, fileContent) {
-    response.writeHead(
-        200,
-        {'Content-Type': mime.lookup(path.basename(filePath))}
-    );
-    response.end(fileContent);
+	response.writeHead(
+		200, {
+			"Content-Type": mime.lookup(path.basename(filePath))
+		}
+	);
+	response.end(fileContent);
 }
 
 function serveStatic(response, cache, absolutePath) {
-    if (cache[absolutePath]) {
-        sendFile(response, absolutePath, cache[absolutePath]);
-    } else {
-        fs.exists(absolutePath, function(exists) {
-            if (exists) {
-                fs.readFile(absolutePath, function(error, data) {
-                    if (error) {
-                        send404(response);
-                    } else {
-                        cache[absolutePath] = data;
-                        sendFile(response, absolutePath, data);
-                    }
-                });
-            } else {
-                send404(response);
-            }
-        });
-    }
+	if (cache[absolutePath]) {
+		sendFile(response, absolutePath, cache[absolutePath]);
+	} else {
+		fs.exists(absolutePath, function(exists) {
+			if (exists) {
+				fs.readFile(absolutePath, function(error, data) {
+					if (error) {
+						send404(response);
+					} else {
+						cache[absolutePath] = data;
+						sendFile(response, absolutePath, data);
+					}
+				});
+			} else {
+				send404(response);
+			}
+		});
+	}
 }
 
 var server = http.createServer(function(request, response) {
-    if (request.url === '/today') {
-        var startDate = new Date();
-        var endDate = new Date();
-        startDate.setHours(0, 0, 0, 0);
-        endDate.setHours(23, 59, 59, 999);
+	if (request.url === "/today") {
+		var startDate = new Date();
+		var endDate = new Date();
+		startDate.setHours(0, 0, 0, 0);
+		endDate.setHours(23, 59, 59, 999);
 
-        nedb.find({"timestamp": {$gte: startDate, $lte: endDate}}).sort({"timestamp": 1}).exec(function(error, logs) {
-            response.writeHead(200, {"Content-Type": "application/json"});
-            response.end(JSON.stringify({"logs": logs}));
-        });
-    } else {
-        var filePath = false;
+		nedb.find({
+			"timestamp": {
+				$gte: startDate,
+				$lte: endDate
+			}
+		}).sort({
+			"timestamp": 1
+		}).exec(function(error, logs) {
+			response.writeHead(200, {
+				"Content-Type": "application/json"
+			});
+			response.end(JSON.stringify({
+				"logs": logs
+			}));
+		});
+	} else {
+		var filePath = false;
 
-        if (request.url === '/') {
-            filePath = 'public/index.html';
-        } else {
-            filePath = 'public' + request.url;
-        }
+		if (request.url === "/") {
+			filePath = "public/index.html";
+		} else {
+			filePath = "public" + request.url;
+		}
 
-        var absolutePath = __dirname + '/' + filePath;
-        serveStatic(response, cache, absolutePath);
-    }
+		var absolutePath = __dirname + "/" + filePath;
+		serveStatic(response, cache, absolutePath);
+	}
 });
 
 
 // Here we go!
 server.listen(80, function() {
-    console.log("Server listening on port 80.");
+	console.log("Server listening on port 80...");
 });
